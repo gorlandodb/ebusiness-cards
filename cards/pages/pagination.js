4da62cb5f741fa8f6369c178e4fd18a4c2532527
christian-cazarin <79495405+christian-cazarin@users.noreply.github.com>
2021-07-23 11:55:04 -0500

    inicio de paginación

---
+++ cards/pages/pagination.js
@@ -0,0 +1,214 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+//https://www.digitalocean.com/community/tutorials/how-to-build-custom-pagination-with-react-es ...
+
+const LEFT_PAGE = 'LEFT';
+const RIGHT_PAGE = 'RIGHT';
+
+/**
+ * Helper method for creating a range of numbers
+ * range(1, 5) => [1, 2, 3, 4, 5]
+ */
+const range = (from, to, step = 1) => {
+  let i = from;
+  const range = [];
+
+  while (i <= to) {
+    range.push(i);
+    i += step;
+  }
+
+  return range;
+}
+
+class Pagination extends Component {
+  constructor(props) {
+    super(props);
+    const { totalRecords = null, pageLimit = 30, pageNeighbours = 0 } = props;
+
+    this.pageLimit = typeof pageLimit === 'number' ? pageLimit : 30;
+    this.totalRecords = typeof totalRecords === 'number' ? totalRecords : 0;
+
+    // pageNeighbours can be: 0, 1 or 2
+    this.pageNeighbours = typeof pageNeighbours === 'number'
+      ? Math.max(0, Math.min(pageNeighbours, 2))
+      : 0;
+
+    this.totalPages = Math.ceil(this.totalRecords / this.pageLimit);
+
+    this.state = { currentPage: 1 };
+  }
+    // ...
+
+  /**
+   * Let's say we have 10 pages and we set pageNeighbours to 2
+   * Given that the current page is 6
+   * The pagination control will look like the following:
+   *
+   * (1) < {4 5} [6] {7 8} > (10)
+   *
+   * (x) => terminal pages: first and last page(always visible)
+   * [x] => represents current page
+   * {...x} => represents page neighbours
+   */
+   fetchPageNumbers = () => {
+    const totalPages = this.totalPages;
+    const currentPage = this.state.currentPage;
+    const pageNeighbours = this.pageNeighbours;
+
+    /**
+     * totalNumbers: the total page numbers to show on the control
+     * totalBlocks: totalNumbers + 2 to cover for the left(<) and right(>) controls
+     */
+    const totalNumbers = (this.pageNeighbours * 2) + 3;
+    const totalBlocks = totalNumbers + 2;
+
+    if (totalPages > totalBlocks) {
+      const startPage = Math.max(2, currentPage - pageNeighbours);
+      const endPage = Math.min(totalPages - 1, currentPage + pageNeighbours);
+      let pages = range(startPage, endPage);
+
+      /**
+       * hasLeftSpill: has hidden pages to the left
+       * hasRightSpill: has hidden pages to the right
+       * spillOffset: number of hidden pages either to the left or to the right
+       */
+      const hasLeftSpill = startPage > 2;
+      const hasRightSpill = (totalPages - endPage) > 1;
+      const spillOffset = totalNumbers - (pages.length + 1);
+
+      switch (true) {
+        // handle: (1) < {5 6} [7] {8 9} (10)
+        case (hasLeftSpill && !hasRightSpill): {
+          const extraPages = range(startPage - spillOffset, startPage - 1);
+          pages = [LEFT_PAGE, ...extraPages, ...pages];
+          break;
+        }
+
+        // handle: (1) {2 3} [4] {5 6} > (10)
+        case (!hasLeftSpill && hasRightSpill): {
+          const extraPages = range(endPage + 1, endPage + spillOffset);
+          pages = [...pages, ...extraPages, RIGHT_PAGE];
+          break;
+        }
+
+        // handle: (1) < {4 5} [6] {7 8} > (10)
+        case (hasLeftSpill && hasRightSpill):
+        default: {
+          pages = [LEFT_PAGE, ...pages, RIGHT_PAGE];
+          break;
+        }
+      }
+
+      return [1, ...pages, totalPages];
+    }
+
+    return range(1, totalPages);
+  }
+  // ...
+
+  render() {
+    if (!this.totalRecords || this.totalPages === 1) return null;
+
+    const { currentPage } = this.state;
+    const pages = this.fetchPageNumbers();
+
+    return (
+      <div id="pagination"
+      className="w-full flex justify-center border-t border-gray-100 pt-4 items-center">
+        { pages.map((page, index) => {
+
+          if (page === LEFT_PAGE) return (
+            <svg
+            className="h-6 w-6"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            onClick={this.handleMoveLeft}
+            >
+              <g opacity="0.4">
+                  <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M9 12C9 12.2652 9.10536 12.5196 9.29289 12.7071L13.2929 16.7072C13.6834 17.0977 14.3166 17.0977 14.7071 16.7072C15.0977 16.3167 15.0977 15.6835 14.7071 15.293L11.4142 12L14.7071 8.70712C15.0977 8.31659 15.0977 7.68343 14.7071 7.29289C14.3166 6.90237 13.6834 6.90237 13.2929 7.29289L9.29289 11.2929C9.10536 11.4804 9 11.7348 9 12Z"
+                  fill="#2C2C2C"
+                  />
+              </g>
+            </svg>
+          );
+
+          if (page === RIGHT_PAGE) return (
+            <svg
+            className="h-6 w-6"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            onClick={this.handleMoveRight}
+            >
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M15 12C15 11.7348 14.8946 11.4804 14.7071 11.2929L10.7071 7.2929C10.3166 6.9024 9.6834 6.9024 9.2929 7.2929C8.9024 7.6834 8.9024 8.3166 9.2929 8.7071L12.5858 12L9.2929 15.2929C8.9024 15.6834 8.9024 16.3166 9.2929 16.7071C9.6834 17.0976 10.3166 17.0976 10.7071 16.7071L14.7071 12.7071C14.8946 12.5196 15 12.2652 15 12Z"
+                fill="#18A0FB"
+            />
+            </svg>
+          );
+
+          return (
+            <p className="leading-relaxed cursor-pointer mx-2 text-sm hover:text-blue-600" onClick={ this.handleClick(page) }>
+            { page }
+            </p>
+          );
+
+        }) }
+      </div>
+    );
+  }
+  // ...
+
+  componentDidMount() {
+    this.gotoPage(1);
+  }
+
+  gotoPage = page => {
+    const { onPageChanged = f => f } = this.props;
+    const currentPage = Math.max(0, Math.min(page, this.totalPages));
+    const paginationData = {
+      currentPage,
+      totalPages: this.totalPages,
+      pageLimit: this.pageLimit,
+      totalRecords: this.totalRecords
+    };
+
+    this.setState({ currentPage }, () => onPageChanged(paginationData));
+  }
+
+  handleClick = page => evt => {
+    evt.preventDefault();
+    this.gotoPage(page);
+  }
+
+  handleMoveLeft = evt => {
+    evt.preventDefault();
+    this.gotoPage(this.state.currentPage - (this.pageNeighbours * 2) - 1);
+  }
+
+  handleMoveRight = evt => {
+    evt.preventDefault();
+    this.gotoPage(this.state.currentPage + (this.pageNeighbours * 2) + 1);
+  }
+}
+
+Pagination.propTypes = {
+  totalRecords: PropTypes.number.isRequired,
+  pageLimit: PropTypes.number,
+  pageNeighbours: PropTypes.number,
+  onPageChanged: PropTypes.func
+};
+
+export default Pagination;
